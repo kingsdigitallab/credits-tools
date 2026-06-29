689f97e36ee3f33bf6d7ea2dd7a50c4757ee7341
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-06-29 12:41:00 +0100

    refactor: simplify and disambiguated namings of model classes in crosswalk

--- crosswalks/codemeta_to_citation_cff.py
+++ crosswalks/codemeta_to_citation_cff.py
@@ -1,28 +1,27 @@
-from pydantic_yaml import to_yaml_str
-from models.codemeta.v3_1.codemeta_pydantic_desc import Software
-from models.citation_cff.v1_2_0.citation_cff_pydantic import CitationFileFormat, Person, Entity
+import models.codemeta.v3_1.codemeta_pydantic_desc as cm
+import models.citation_cff.v1_2_0.citation_cff_pydantic as cff
 
-def get_cff_agent(agent):
+def get_cff_agent(agent: cm.Organization | cm.Person):
     if agent.type_ == 'Organization':
         ret = {
             "name": agent.name,
         }
-        ret = Entity(**ret)
+        ret = cff.Entity(**ret)
     else:
         ret = {
             "given-names": agent.givenName,
             "family-names": agent.familyName,
         }
-        ret = Person(**ret)
+        ret = cff.Person(**ret)
     return ret
 
-def get_citation_file_format_from_codemeta_software(codemeta_software: Software) -> CitationFileFormat:
+def get_citation_file_format_from_codemeta_software(codemeta_software: cm.Software) -> cff.CitationFileFormat:
     soft = codemeta_software
     
-    cff = {
+    cff_data = {
         "authors": [get_cff_agent(a) for a in soft.author],
         "title": soft.name,
         "repository": soft.codeRepository
     }
 
-    return CitationFileFormat(**cff)
+    return cff.CitationFileFormat(**cff_data)
